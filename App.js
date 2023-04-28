0506787341554c6a944cfa19bd45b4387d684977
niharika <niharikadsn@gmail.com>
2023-04-28 20:55:23 +0530

    episode 1

---
+++ App.js
@@ -0,0 +1,24 @@
+    //*** In JavaScript ***//
+    
+    /* const heading = document.createElement("h1");
+    heading.innerHTML = "hello world!"
+    
+    const root = document.getElementById("root");
+    root.appendChild(heading); */
+
+    //*** In React ***//
+
+    const heading = React.createElement("h1",
+    {id:"heading"}, 
+    "Hello World from React"); 
+    
+    /* ==> 
+    React.createElement("element tag", {attributes}, child element/ [array of child element tags]) 
+    <== */
+
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+
+    root.render(heading);
+
+
+    
---
+++ README.md
@@ -0,0 +1,8 @@
+# GIT Instructions
+
+-> to initialise git : git init
+-> to create  branch and make it as a main branch : git branch -M main
+/**  PUSH code to GitHub **/
+-> to add all the files : git add .
+-> to commit the code with a message : git commit -m "episode 1"
+
